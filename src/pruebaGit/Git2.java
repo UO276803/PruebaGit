7692e7c39ad174f6bc2f807558c891b1767f73af
Usuario <Usuario@DESKTOP-RM7GP31>
2022-09-24 20:37:27 +0200

    Cambio en rama2

--- src/pruebaGit/Git2.java
+++ src/pruebaGit/Git2.java
@@ -4,6 +4,8 @@ public class Git2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		String cambio = "Hola estoy cambiando de rama";
+		System.out.println(cambio);
 	}
 
 }
